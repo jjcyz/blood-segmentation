496d1190069eeeedcd8d59057bc42adaf294729a
Jessica <jess.c.zhou@gmail.com>
2025-05-05 22:22:22 -0700

    Implemented Conv Block

--- src/model.py
+++ src/model.py
@@ -1,1 +1,7 @@
+import tensorflow as tf
 
+def conv_block(x, filters):
+  x = tf.keras.layers.Conv3D(filters, 3, padding='same')(x)
+  x = tf.keras.layers.BatchNormalization()(x)
+  x = tf.keras.layers.ReLU()(x)
+  return x
