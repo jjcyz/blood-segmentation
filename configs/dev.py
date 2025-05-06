1fe67e56d6c5ac47c0dd0737d1db95f0f3b2a1f0
Jessica <jess.c.zhou@gmail.com>
2025-05-05 22:07:10 -0700

    Initial project Setup

---
+++ configs/dev.py
@@ -0,0 +1,10 @@
+# configs/dev.py
+DEV_CONFIG = {
+    'PATCH_WIDTH': 32,      # Smaller for testing
+    'PATCH_HEIGHT': 32,
+    'PATCH_DEPTH': 8,
+    'BATCH_SIZE': 2,
+    'EPOCHS': 2,
+    'NUM_WORKERS': 2,
+    'CACHE_SIZE': 10
+}
---
+++ configs/production.py
@@ -0,0 +1,10 @@
+# configs/production.py
+PRODUCTION_CONFIG = {
+    'PATCH_WIDTH': 64,
+    'PATCH_HEIGHT': 64,
+    'PATCH_DEPTH': 16,
+    'BATCH_SIZE': 8,
+    'EPOCHS': 50,
+    'NUM_WORKERS': 4,
+    'CACHE_SIZE': 150
+}
