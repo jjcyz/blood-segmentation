0c623fc3ff30c0b025abbcde1a17c487fe3c52a5
Jessica <jess.c.zhou@gmail.com>
2025-05-07 14:26:43 -0700

    Metrics Recall

--- src/metrics.py
+++ src/metrics.py
@@ -18,5 +18,12 @@ class MetricsCollection:
     predicted_positives = K.sum(K.round(K.clip(y_pred ,0, 1)))
     return true_positives / (predicted_positives + K.epsilon())
 
+  @staticmethod
+  def recall(y_true, y_pred):
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
 
 
