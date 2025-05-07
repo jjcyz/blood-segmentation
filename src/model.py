c854622c7c5530b72115deeb57d031120f886000
Jessica <jess.c.zhou@gmail.com>
2025-05-06 22:57:49 -0700

    Implemented Decoder

--- src/model.py
+++ src/model.py
@@ -25,6 +25,17 @@ def create_memory_efficient_unet(input_shape):
   conv3 = conv_block(pool2, 128)
 
   # Decoder
+  up2 = tf.keras.layers.Conv3DTranspose(64, 2, strides=(2, 2, 2), padding='same')(conv3)
+  up2 = tf.keras.layers.concatenate([conv2, up2])
+  conv4 = conv_block(up2, 64)
+
+  up1 = tf.keras.layers.Conv3DTranspose(32, 2, strides=(2, 2, 2), padding='same')(conv4)
+  up1 = tf.keras.layers.concatenate([conv1, up1])
+  conv5 = conv_block(up1, 32)
+
+  outputs = tf.keras.layers.Conv3D(1, 1, activation='sigmoid')(conv5)
+
+  return tf.keras.Model(input=[inputs], outputs=[outputs])
 
 
 
